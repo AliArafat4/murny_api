24a0bc9410b50bd7924186d4e9ed6b54c9b262ec
Zexion16 <alihma188@gmail.com>
2023-12-26 10:31:14 +0300

    adding get drivers endpoint

--- bin/routes/common_route.dart
+++ bin/routes/common_route.dart
@@ -1,11 +1,14 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+import '../handlers/common_handlers/get_all_drivers_handler.dart';
 import '../handlers/common_handlers/get_order_handler.dart';
 import '../middlewares/user_middleware.dart';
 
 class CommonRoute {
   Handler get route {
-    final Router appRoute = Router()..get("/get_order", getOrderHandler);
+    final Router appRoute = Router()
+      ..get("/get_order", getOrderHandler)
+      ..get("/get_drivers", getAllDriversHandler);
 
     final pipe =
         Pipeline().addMiddleware(checkToken()).addHandler(appRoute.call);
